05ea5e7742db661585ab0407bacf602313df5a8d
isasan <isabelsanle@gmail.com>
2015-08-15 14:07:35 +0200

    Moderación de Comentarios

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+router.param('commentId', commentController.load);  // autoload :commentId
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
@@ -29,6 +30,8 @@ router.delete('/quizes/:quizId(\\d+)',sessionController.loginRequired,quizContro
 
 router.get('/quizes/:quizId(\\d+)/comments/new',commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
+
 
 // Página autor
 router.get('/author', function (req, res, next) {res.render('author', {errors:[]});});
